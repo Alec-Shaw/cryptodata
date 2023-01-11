a38facf5f2411f5c24b9be62a6a8571aa6596a69
Alec_Shaw <oleg.iliushov@gmail.com>
2023-01-11 17:26:07 +0300

    change date

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,42 +1,43 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
   //  'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
   //  'X-RapidAPI-Host': 'bloomberg-market-and-financial-news.p.rapidapi.com'
-      
-    
-}
+};
 
-const baseUrl = 'https://api.coinpaprika.com/v1';
+const baseUrl = "https://api.coinpaprika.com/v1";
 
-const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-      getCryptos: builder.query({  
-        query: () => createRequest('/global'),
-      }),
-      getCryptoCoins: builder.query({  
-        query: (count) => createRequest(`/tickers?limit=${count}`),
-      }),
-      getCryptoDetails: builder.query({  
-        query: (coinId) => createRequest(`/tickers/${coinId}`),
-      }),
-      getCryptoHistory: builder.query({  
-        query: ({coinId, timePeriod}) => createRequest(`/tickers/${coinId}/historical?start=2022-01-01&interval=${timePeriod}`),
-      }),
-      getCryptoExchange: builder.query({  
-        query: () => createRequest(`/exchanges`),
-      })         
-    })
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/global"),
+    }),
+    getCryptoCoins: builder.query({
+      query: (count) => createRequest(`/tickers?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/tickers/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(
+          `/tickers/${coinId}/historical?start=2022-09-09&interval=${timePeriod}`
+        ),
+    }),
+    getCryptoExchange: builder.query({
+      query: () => createRequest(`/exchanges`),
+    }),
+  }),
 });
 
 export const {
-    useGetCryptosQuery,
-    useGetCryptoCoinsQuery,
-    useGetCryptoDetailsQuery,
-    useGetCryptoHistoryQuery,
-    useGetCryptoExchangeQuery
+  useGetCryptosQuery,
+  useGetCryptoCoinsQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+  useGetCryptoExchangeQuery,
 } = cryptoApi;
